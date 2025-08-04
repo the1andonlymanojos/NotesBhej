844578bec9275ef915cca2d7676aef7acc056d81
Manoj Shiv <115482304+the1andonlymanojos@users.noreply.github.com>
2025-08-04 13:16:44 +0530

    Update searchbox.tsx
    added import

--- src/components/searchbox.tsx
+++ src/components/searchbox.tsx
@@ -3,7 +3,7 @@ import { DialogTitle } from "@radix-ui/react-dialog"
 import { Search, BookOpen, ArrowRight, Plus } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { useRouter } from "next/navigation"
-import { useState } from "react"
+import { useState, useMemo } from "react"
 import { Database } from "@/types/supabase";
 
 type CourseNew = Database['public']['Tables']['coursenew']['Row']
