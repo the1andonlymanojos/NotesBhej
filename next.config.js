f19586b90fd0160b9113f90c2e7418804257af72
the1andonlymanojos <mshivagange@gmail.com>
2025-09-21 09:42:05 +0530

    - removed console logs in production, shouldve done it a long time ago.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', 'data.miga.manoj-shiv.tech', 'avatars.githubusercontent.com','i.scdn.co', 'static-cdn.jtvnw.net', 'cdn.discordapp.com'], // Allow Google user content images
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',  
+    // or exclude certain types:
+    removeConsole: { exclude: ['error'] }
+  },
   productionBrowserSourceMaps: true
 }
 
